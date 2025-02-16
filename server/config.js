8edb53ba9c4a34c03c17d6602618d519f4e41b8f
Ramya-dev9 <ramyabujji9412@gmail.com>
2025-02-16 18:36:26 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,9 +3,9 @@
 
 
 module.exports = {
-  database: 'mongodb+srv://ramyarobin779:33zrzDgL_uAXC_f@amo.iff9f.mongodb.net/?retryWrites=true&w=majority&appName=amo',
+  database: '',
   port: 3030,
-  secret: 'c4f3dfe12b1e4e84a2b3b50e2c9e7d16342c1b3e4b5d77d6a6f6e63e3f8a7b9d'
+  secret: ''
 };
 
 
